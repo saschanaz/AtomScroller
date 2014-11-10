cb70315d995358a1ed41d2aa028a454fb7d3cc50
SaschaNaz <saschanaz@outlook.com>
2014-11-10 22:32:59 +0900

    much cleaner scroll

--- lib/sources/atom-scroller.ts
+++ lib/sources/atom-scroller.ts
@@ -2,15 +2,6 @@
 ///<reference﻿ path="atom-scroller-extensions.d.ts" />
 
 module AtomScroller {
-  var regex = /translate3d\(0px, -?([0-9]*)px, 0px/;
-  function transformToScroll(view: HTMLElement, linenumber: HTMLElement) {
-    var test = linenumber.style.webkitTransform.match(regex);
-    if (test.length < 1)
-      return;
-    var scroll = parseInt(linenumber.style.webkitTransform.match(regex)[1]);;
-    view.scrollTop = scroll;
-  }
-
   export function patchEditor(editorView: any) {// EditorView
     Array.prototype.forEach.call(editorView.element.querySelectorAll(".horizontal-scrollbar, .vertical-scrollbar"),
       (bar: HTMLElement) => bar.style.display = "none");
@@ -22,22 +13,19 @@ module AtomScroller {
     content.style.transform = "translate3d(0px, 0px, 0px)";
 
     view.removeEventListener("scroll", editorView.component.onScrollViewScroll);
-    element.querySelector(".editor-contents").removeEventListener("mousewheel", editorView.component.onMouseWheel);
     // Getting our scrollbars here.
     view.style.overflow = "auto";
 
     view.addEventListener("scroll", function () {
       // Match scroll position when touch-scrolled
       linenumbers.style.webkitTransform = "translate3d(0px, -" + view.scrollTop + "px, 0px)";
-      editorView.editor.displayBuffer.setScrollTop(view.scrollTop);
+      editorView.editor.setScrollTop(view.scrollTop);
     });
 
-    // Match scroll position when cursor-scrolled
-    transformToScroll(view, linenumbers);
-    var viewobserver = new MutationObserver(function () {
-      transformToScroll(view, linenumbers);
-    });
-    viewobserver.observe(linenumbers, { attributes: true });
+    view.scrollTop = editorView.editor.getScrollTop();
+    view.scrollLeft = editorView.editor.getScrollLeft();
+    editorView.editor.onDidChangeScrollTop((top: number) => view.scrollTop = top);
+    editorView.editor.onDidChangeScrollLeft((left: number) => view.scrollLeft = left);
   }
 }
 
