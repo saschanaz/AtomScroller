fd066ae6c09d4b6ce08a7f82ecdb82ad0e36f51c
SaschaNaz <saschanaz@outlook.com>
2014-11-11 17:42:31 +0900

    deactivation

--- lib/sources/atom-scroller-extensions.d.ts
+++ lib/sources/atom-scroller-extensions.d.ts
@@ -1,3 +1,17 @@
 interface MSStyleCSSProperties {
   webkitTransform: string;
 }
+
+interface IScrollView extends HTMLElement {
+  _atomscroller_onscroll?: any;
+}
+
+declare module eventKit {
+  class Disposable {
+    disposed: boolean;
+
+    constructor(disposalAction: any);
+    dispose(): void;
+    off(): void;
+  }
+}
--- lib/sources/atom-scroller.ts
+++ lib/sources/atom-scroller.ts
@@ -2,13 +2,26 @@
 ///<reference﻿ path="atom-scroller-extensions.d.ts" />
 
 module AtomScroller {
-  export function patchEditor(editorView: any) {// EditorView
-    Array.prototype.forEach.call(editorView.element.querySelectorAll(".horizontal-scrollbar, .vertical-scrollbar"),
-      (bar: HTMLElement) => bar.style.display = "none");
+  export var subscriptions: eventKit.Disposable[] = [];
 
-    var view = editorView.element.querySelector(".scroll-view");
-    var linenumbers = editorView.gutter.find(".line-numbers")[0];
-    var content = editorView.element.querySelector(".lines.overlayer");
+  function getElements(editorView: any) {
+    return {
+      view: <IScrollView>editorView.element.querySelector(".scroll-view"),
+      linenumbers: editorView.gutter.find(".line-numbers")[0],
+      content: editorView.element.querySelector(".lines.overlayer"),
+      scrollbars: editorView.element.querySelectorAll(".horizontal-scrollbar, .vertical-scrollbar")
+    };
+  }
+
+  export function patchEditor(editorView: any) {// EditorView
+    var elements = getElements(editorView);
+    var view = elements.view;
+    var linenumbers = elements.linenumbers;
+    var content = elements.content;
+    var scrollbars = elements.scrollbars;
+
+    Array.prototype.forEach.call(scrollbars, (bar: HTMLElement) => bar.style.display = "none");
+
     // Preventing any further transforms, by suppressing -webkit- prefixed property.
     content.style.transform = "translate3d(0px, 0px, 0px)";
 
@@ -16,23 +29,51 @@ module AtomScroller {
     // Getting our scrollbars here.
     view.style.overflow = "auto";
 
-    view.addEventListener("scroll", function () {
+    view._atomscroller_onscroll = () => {
       // Match scroll position when touch-scrolled
       linenumbers.style.webkitTransform = "translate3d(0px, -" + view.scrollTop + "px, 0px)";
       editorView.editor.setScrollTop(view.scrollTop);
-    });
+    };
+    view.addEventListener("scroll", view._atomscroller_onscroll);
 
     view.scrollTop = editorView.editor.getScrollTop();
     view.scrollLeft = editorView.editor.getScrollLeft();
-    editorView.editor.onDidChangeScrollTop((top: number) => view.scrollTop = top);
-    editorView.editor.onDidChangeScrollLeft((left: number) => view.scrollLeft = left);
+    subscriptions.push(editorView.editor.onDidChangeScrollTop((top: number) => view.scrollTop = top));
+    subscriptions.push(editorView.editor.onDidChangeScrollLeft((left: number) => view.scrollLeft = left));
+    console.log(subscriptions);
+  }
+
+  export function depatchEditor(editorView: any) {
+    var elements = getElements(editorView);
+    var view = elements.view;
+    var linenumbers = elements.linenumbers;
+    var content = elements.content;
+    var scrollbars = elements.scrollbars;
+
+    Array.prototype.forEach.call(scrollbars, (bar: HTMLElement) => bar.style.display = "");
+
+    content.style.transform = "";
+
+    view.addEventListener("scroll", editorView.component.onScrollViewScroll);
+
+    view.style.overflow = "";
+
+    view.removeEventListener("scroll", view._atomscroller_onscroll);
   }
 }
 
 export var activate = (state: AtomCore.IAtomState) => {
-  atom.workspaceView.eachEditorView((editorView) => AtomScroller.patchEditor(editorView));
+  AtomScroller.subscriptions.push(<any>atom.workspaceView.eachEditorView((editorView) => AtomScroller.patchEditor(editorView)));
 }
 
 export var deactivate = () => {
-
+  atom.workspaceView.getEditorViews().forEach((editorView) => AtomScroller.depatchEditor(editorView));
+  console.log(AtomScroller.subscriptions)
+  AtomScroller.subscriptions.forEach((subscription) =>  {
+    if (subscription.dispose)
+      subscription.dispose();
+    else
+      subscription.off();
+  });
+  AtomScroller.subscriptions = [];
 }
